9be5a29e325cb400513995520b9437d2ebbf3951
fullcycle <dfoderick@gmail.com>
2020-06-23 19:29:22 -0700

    test attempts broadcast

--- test/functional/simplespend.test.ts
+++ test/functional/simplespend.test.ts
@@ -7,26 +7,24 @@ describe('wallet broadcasts simple spend', () => {
         expect(w).toBeInstanceOf(Wallet)
         w.loadWallet('L5o1VbLNhELT6uCu8v7KdZpvVocHWnHBqaHe686ZkMkyszyU6D7n')
         const nftx = await w.makeAnyoneCanSpendTx(Long.fromNumber(100))
-        //console.log(w.lastTx.toJSON())
-        //cannot get input satoshis?
         expect(w.lastTx.txIns.length).toBe(1)
     })
 
-    // it('broadcasts', async () => {
-    //     const sender = new Wallet()
-    //     sender.loadWallet('L5o1VbLNhELT6uCu8v7KdZpvVocHWnHBqaHe686ZkMkyszyU6D7n')
-    //     sender.logDetails()
-    //     let sender_hex, sent
-    //     sender_hex = await sender.makeAnyoneCanSpendTx(
-    //         Long.fromNumber(600)
-    //     )
-    //     //TODO: might not be 1 if multiple utxo combined
-    //     expect(sender.lastTx.inputs.length).toBe(1)
-    //     console.log(sender_hex)
-    //     console.log(sender.logDetails(sender.lastTx))
-    //     // sent = await sender.broadcastRaw(sender_hex)
-    //     // console.log(`broadcast Tx ${sent}`)
-    //     // //result should be 32 byte hex
-    //     // expect(sent.length).toBe(32)
-    // },10000)
+    it('broadcasts', async () => {
+        const sender = new Wallet()
+        sender.loadWallet('L5o1VbLNhELT6uCu8v7KdZpvVocHWnHBqaHe686ZkMkyszyU6D7n')
+        sender.logDetails()
+        let sender_hex, sent
+        sender_hex = await sender.makeAnyoneCanSpendTx(
+            Long.fromNumber(600)
+        )
+        //TODO: might not be 1 if multiple utxo combined
+        expect(sender.lastTx.txIns.length).toBe(1)
+        sender.logDetailsLastTx()
+        console.log(sender_hex)
+        sent = await sender.broadcastRaw(sender_hex)
+        console.log(`broadcast Tx ${sent}`)
+        //result should be 32 byte hex
+        expect(sent.length).toBe(32)
+    },10000)
 })
