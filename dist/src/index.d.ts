6ab5c561d3cac5b73ed2c9442a56ad97b96e3127
fullcycle <dfoderick@gmail.com>
2020-06-29 17:52:10 -0700

    export interfaces

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -3,4 +3,6 @@ export { TransactionBuilder } from './TransactionBuilder';
 export { KeyPair } from './KeyPair';
 export { UnspentOutput } from './UnspentOutput';
 export { OutputCollection } from './OutputCollection';
+export { IStorage } from './FileSystemStorage';
+export { IIndexingService } from './IndexingService';
 //# sourceMappingURL=index.d.ts.map
